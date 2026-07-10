d5554f4a960cba0c41ae32fccb33322968ba5f35
andrecordeirol <lordbironbr@yahoo.com.br>
2026-07-10 08:43:40 -0300

    Project hr-payroll created

---
+++ hs-payroll/src/main/java/com/devsuperior/hspayroll/HsPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hspayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsPayrollApplication.class, args);
+    }
+
+}
---
+++ hs-payroll/src/test/java/com/devsuperior/hspayroll/HsPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hspayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
