db5cbf499ab4c66b1c77ac8ced4a7f1b87491514
andrecordeirol <lordbironbr@yahoo.com.br>
2026-08-06 09:11:42 -0300

    Hystrix fall tolerance

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/resources/PaymentResource.java
@@ -16,9 +16,21 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+    //@HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
+
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
-        Payment payment = service.getPayment(workerId, days);
-        return ResponseEntity.ok(payment);
+        try {
+            Payment payment = service.getPayment(workerId, days);
+            return ResponseEntity.ok(payment);
+        } catch (Exception e) {
+                Payment payment = new Payment("Brann", 400.0, days);
+                return ResponseEntity.ok(payment);
+        }
     }
+    /*
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
+        return ResponseEntity.ok(payment);
+    }*/
 }
