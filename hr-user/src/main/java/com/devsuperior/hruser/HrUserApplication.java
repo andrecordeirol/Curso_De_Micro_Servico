5e73b98a9278ad43836a4ca0a947bc5a9cce8b47
andrecordeirol <andrecordeirolacerda@gmail.com>
2026-09-10 11:11:31 -0300

    User, Role, many to many association

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -2,7 +2,10 @@ package com.devsuperior.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
