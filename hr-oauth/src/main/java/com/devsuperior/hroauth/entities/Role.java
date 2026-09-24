311af84bd00541520cabdf900a54b2433ac7b533
andrecordeirol <andrecordeirolacerda@gmail.com>
2026-09-24 14:58:35 -0300

    User feing client

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/Role.java
@@ -0,0 +1,47 @@
+package com.devsuperior.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String roleName;
+
+    public Role(){
+    }
+
+    public Role(Long id, String roleNane) {
+        this.id = id;
+        this.roleName = roleNane;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(roleName);
+    }
+}
---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,30 @@
+package com.devsuperior.hroauth.services;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feignclients.UserFeingClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserFeingClient userFeingClient;
+
+    public User findByEmail(String email) {
+
+        User user = userFeingClient.findByEmail(email).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + email);
+            throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found" + email);
+        return user;
+    }
+
+
+}
